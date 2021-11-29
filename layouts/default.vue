696c32cfc2d05444e9241f1f4a688edc06430a45
Jenifer Morati Betti <jenifermorati26@gmail.com>
2021-11-29 03:29:02 -0300

    feat(estructure): create estructure nuxt

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app @changeMsg="setMessage">
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-icon> mdi-camera-burst </v-icon>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-footer :absolute="!fixed" app>
+      <span>{{ title }} &copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fixed: false,
+      title: 'GridPics',
+    }
+  },
+}
+</script>
