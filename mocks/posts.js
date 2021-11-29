9361bb598f3f00576f1b0588e96b9f5be0938218
Jenifer Morati Betti <jenifermorati26@gmail.com>
2021-11-29 03:32:12 -0300

    feat(mocks): add new mocks

---
+++ mocks/posts.js
@@ -0,0 +1,76 @@
+const posts = [
+  {
+    id: 0,
+    title: 'As águas do mar',
+    img: 'https://images.pexels.com/photos/533923/pexels-photo-533923.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: 'Não é doce mas é bela.',
+  },
+  {
+    id: 1,
+    title: 'Cobertos pela neve',
+    img: 'https://images.pexels.com/photos/3540375/pexels-photo-3540375.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: 'Como se fosse um cobertor geladinho',
+  },
+  {
+    id: 2,
+    title: 'Um mar de areia',
+    img: 'https://images.pexels.com/photos/9335688/pexels-photo-9335688.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: 'São como ondas, só que de areia',
+  },
+  {
+    id: 3,
+    title: 'Chuveiro eco',
+    img: 'https://images.pexels.com/photos/9808222/pexels-photo-9808222.png?auto=compress&cs=tinysrgb&dpr=1&w=500',
+    description: 'Que banho gelado!.',
+  },
+  {
+    id: 4,
+    title: 'Cobertos pela neve',
+    img: 'https://images.pexels.com/photos/4561714/pexels-photo-4561714.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: 'Como se fosse um cobertor geladinho',
+  },
+  {
+    id: 5,
+    title: 'Post 3',
+    img: 'https://images.pexels.com/photos/33688/delicate-arch-night-stars-landscape.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'Esse é o post 3',
+  },
+  {
+    id: 6,
+    title: 'As águas do mar',
+    img: 'https://images.pexels.com/photos/3342739/pexels-photo-3342739.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'Não é doce mas é bela.',
+  },
+  {
+    id: 7,
+    title: 'Cobertos pela neve',
+    img: 'https://images.pexels.com/photos/2385210/pexels-photo-2385210.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    description: 'Como se fosse um cobertor geladinho',
+  },
+  {
+    id: 8,
+    title: 'Post 3',
+    img: 'https://images.pexels.com/photos/707344/pexels-photo-707344.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: 'Esse é o post 3',
+  },
+  {
+    id: 9,
+    title: 'As águas do mar',
+    img: 'https://images.pexels.com/photos/1450082/pexels-photo-1450082.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: 'Não é doce mas é bela.',
+  },
+  {
+    id: 10,
+    title: 'Cobertos pela neve',
+    img: 'https://images.pexels.com/photos/1367192/pexels-photo-1367192.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: 'Como se fosse um cobertor geladinho',
+  },
+  {
+    id: 11,
+    title: 'Post 3',
+    img: 'https://images.pexels.com/photos/7919/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+    description: 'Esse é o post 3',
+  },
+]
+
+export default posts
