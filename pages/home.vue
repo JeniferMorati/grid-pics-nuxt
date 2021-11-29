37d18b7c77517840b8a41207de557a2b4f7677e8
Jenifer Morati Betti <jenifermorati26@gmail.com>
2021-11-29 03:31:05 -0300

    feat(home): create home page for post list

---
+++ pages/home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home">
+    <Post
+      v-for="post in newPosts"
+      v-bind:key="post.id"
+      :title="post.title"
+      :img="post.img"
+      :description="post.description"
+    />
+  </div>
+</template>
+
+<script>
+import posts from '../mocks/posts'
+
+export default {
+  data() {
+    return {
+      newPosts: posts,
+    }
+  },
+  layout: 'logged',
+}
+</script>
+
+<style scoped>
+.home {
+  max-width: 1200px;
+  margin: 0 auto;
+  column-count: 4;
+}
+
+@media (max-width: 1200px) {
+  .home {
+    column-count: 3;
+  }
+}
+
+@media (max-width: 700px) {
+  .home {
+    column-count: 2;
+  }
+}
+
+@media (max-width: 460px) {
+  .home {
+    column-count: 1;
+  }
+}
+</style>
